078c669abcca3bf72c4d34d1f8172df4c903a6e7
ngueagho <ngueaghotiodongrobertolandry@gmail.com>
2023-07-28 07:03:37 -0700

    version 1.3

--- Terraform/exercice1/Terraform/modules/docker/main.tf
+++ Terraform/exercice1/Terraform/modules/docker/main.tf
@@ -27,9 +27,37 @@ resource "docker_image" "httpd-port8000" {
 }
  /*Faite un push de l’image dans un docker registry à l’aide de terraform  */
 
-resource "nu" "name" {
-  
+resource "null_resource" "docker_tag_nginx" {
+  provisioner "local-exec" {
+    command = "docker tag newnginx robertolandry/tp18juillet2023-nginx-port5000:latest"
+  }
 }
-resource "null_resource" "docker_hub" {
+resource "null_resource" "docker_tag_httpd" {
+  provisioner "local-exec" {
+    command = "docker tag newhttpd  robertolandry/tp18juillet2023-httpd-port8000:latest"
+  }
+}
 
+resource "null_resource" "docker_version" {
+  triggers = {
+    image_version= "latest"
+  }
 }
+
+resource "null_resource" "docker_push_nginx" {
+  triggers = {
+    image_version= "latest"
+  }
+  provisioner "local-exec" {
+    command = "docker push robertolandry/tp18juillet2023-nginx-port5000:latest"
+  }
+}
+resource "null_resource" "docker_push_httpd" {
+  triggers = {
+    image_version= "latest"
+  }
+  provisioner "local-exec" {
+    command = "docker push robertolandry/tp18juillet2023-httpd-port8000:latest"
+  }
+}
+
